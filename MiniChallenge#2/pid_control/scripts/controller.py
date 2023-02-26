d1cc5518caf3ac152ae601804b7f648120618fb2
josfrend <75293152+josfrend@users.noreply.github.com>
2023-02-26 17:35:28 -0600

    changed the pid implementation
    using the formula on the description

--- MiniChallenge#2/pid_control/scripts/controller.py
+++ MiniChallenge#2/pid_control/scripts/controller.py
@@ -12,7 +12,12 @@ estado = ""
 adjustment = motor_input()
 set_up_delay = 0
 kp = rospy.get_param("/system_param_Kp")
-ti = rospy.get_param("/system_param_Ti")
+ki = rospy.get_param("/system_param_Ti")
+kd = rospy.get_param("/system_param_Kd")
+
+integral = 0.0
+last_error = 0.0
+last_time = 0.0
 
 #Setup parameters, vriables and callback functions here (if required)
 def callback(msg):
@@ -25,46 +30,23 @@ def callback2(msg):
   referencia = msg.reference
   time = msg.time
 
-e = 0.0
-e_1 = 0.0
-u = 0.0
-u_1 = 0.0
 
-q0 = 0.0
-q1 = 0.0
+def PID_Discrete(feedback, setpoint):
+  global last_error, last_time, integral
+  current_time = time
+  dt = current_time - last_time
 
-def PID_Initialize():
-    global kp, ti, q0, q1
-    
-    t = 0.01    #sample rate
-    
-    # Calculate the coefficients for the discrete PID controller
-    #kp = (0.9 * tao) / (k * theta)
-    #ti = 3.33 * theta
-    q0 = kp + ((kp * t)/(2.0 * ti))
-    q1 = ((kp * t)/(2.0 * ti)) - kp
+  error = setpoint - feedback
+  derivative = (error - last_error)
+  integral += error * dt
 
-def PID_Discrete(yM, max_val, min_val, set_point):
-    global e, e_1, u, u_1
-    
-    # Calculate the error
-    e = set_point - yM
-    
-    # Calculate the discrete PID controller
-    u = u_1 + q0 * e + q1 * e_1
-    
-    # Saturate the controller with upper and lower limits
-    if u >= max_val:
-        u = max_val
-    elif u <= min_val:
-        u = min_val
-    
-    # Update the values for the next iteration
-    e_1 = e
-    u_1 = u
-    
-    return u
-  
+  output = kp * error + ki * integral + kd * derivative
+  last_error = error
+  last_time = current_time
+
+  return output
+
+
 
 #Stop Condition
 def stop():
@@ -77,7 +59,7 @@ if __name__=='__main__':
     rospy.init_node("controller")
     rate = rospy.Rate(100)
     rospy.on_shutdown(stop)
-    PID_Initialize()
+    last_time = time
 
     #Setup Publishers and subscribers here
     pub = rospy.Publisher("motor_input", motor_input, queue_size = 10)
@@ -91,7 +73,7 @@ if __name__=='__main__':
     while not rospy.is_shutdown():
 
         #Write your code here
-      if(set_up_delay >= 1000): adjustment.input = PID_Discrete(salida, 13.0006322861, -13.0006322861, referencia)
+      if(set_up_delay >= 500): adjustment.input = PID_Discrete(salida, referencia)
       else: adjustment.input = 0.0
       adjustment.time = time
       pub.publish(adjustment)
