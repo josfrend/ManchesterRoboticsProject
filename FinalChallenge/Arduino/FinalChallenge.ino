99058cc502c982770e34ac259194aff7751215a6
josfrend <75293152+josfrend@users.noreply.github.com>
2023-03-11 18:37:59 -0600

    Update FinalChallenge.ino

--- FinalChallenge/Arduino/FinalChallenge.ino
+++ FinalChallenge/Arduino/FinalChallenge.ino
@@ -8,10 +8,10 @@
 #define ENCPULSES 13
 
 ros::NodeHandle  nh;
-int pwm_value = 100;
+int pwm_value = 0;
 bool direction;
 volatile int pulse_count;
-int span = 10;
+int span = 12;
 long previous_time = 0;
 long current_time = 0;
 
@@ -20,18 +20,19 @@ float rpm = 0;
 float angular_velocity = 0;
 const float rpm_to_radians = 0.10471975512;
 const float rad_to_deg = 57.29578;
-
+int movingAverage[2];
+int mCount = 0;
 
 std_msgs::Int16 str_msg;
 std_msgs::Float32 omega_msg;
-ros::Publisher chatter("omega", &str_msg);
-ros::Publisher chatter2("omega2", &omega_msg);
 
-void messageCB(const std_msgs::Int16& toogle_msg){
-  pwm_value = toogle_msg.data;
+ros::Publisher chatter("motor_output", &omega_msg);
+
+void messageCB(const std_msgs::Float32& toogle_msg){
+  pwm_value = toogle_msg.data * 255.0;
 }
 
-ros::Subscriber <std_msgs::Int16> sub("cmd_pwm", &messageCB);
+ros::Subscriber <std_msgs::Float32> sub("motor_input", &messageCB);
 
 char hello[13] = "hello world!";
 
@@ -44,7 +45,6 @@ void setup()
   pinMode(ENCB, INPUT);
   nh.initNode();
   nh.advertise(chatter);
-  nh.advertise(chatter2);
   nh.subscribe(sub);
   attachInterrupt(digitalPinToInterrupt(ENCA),ENC_read,RISING);
 }
@@ -52,14 +52,14 @@ void setup()
 void loop()
 {
   str_msg.data = pwm_value;
-  if(pwm_value > 255){
+  if(pwm_value > 0){
+    analogWrite(3,0);
+    analogWrite(4,255);
+    pwm_value = pwm_value;
+  }else if(pwm_value <= 0){
     analogWrite(3,255);
     analogWrite(4,0);
-    pwm_value = pwm_value - 255;
-  }else if(pwm_value <= 255){
-    analogWrite(4,255);
-    analogWrite(3,0);
-    pwm_value = -1 *(pwm_value - 510);
+    pwm_value = -1 *(pwm_value);
   }
   current_time = millis();
   if(current_time-previous_time > span){
@@ -68,9 +68,33 @@ void loop()
     angular_velocity = rpm * rpm_to_radians;
 
     pulse_count = 0;
+    /*
+    if(mCount == 0){
+      for(int i = 0; i <2; i++){
+        movingAverage[i] = angular_velocity;
+      }
+      mCount++;
+    }else{
+      for(int i=1;i<2;i++){
+        movingAverage[i-1] = movingAverage[i];
+      }
+      movingAverage[1] = angular_velocity;
+    }
+
+    angular_velocity = 0;
+    
+    for(int j = 0; j <2 ; j++){
+      angular_velocity += movingAverage[j]; 
+    }
+    angular_velocity = angular_velocity/2;
+    */
   }
+
+  
+  
+ 
   omega_msg.data = angular_velocity;
-  chatter2.publish(&omega_msg);
+  chatter.publish(&omega_msg);
   analogWrite(pwm_pin, pwm_value);
   nh.spinOnce();
   delay(10);
