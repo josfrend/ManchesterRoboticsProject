d4c972f72593c3323b1be83f91bf483cc0e488e8
josfrend <75293152+josfrend@users.noreply.github.com>
2023-04-15 16:04:12 -0600

    Update controller.py

--- Implementacion de Robotica Inteligente/Week 2/puzzlebot_operation/scripts/controller.py
+++ Implementacion de Robotica Inteligente/Week 2/puzzlebot_operation/scripts/controller.py
@@ -31,48 +31,17 @@ def coordinates(coordinate):
   xdf = coordinate[0] - pointSelf[0]  
   ydf = coordinate[1] - pointSelf[1]
   magnitude = np.sqrt(xdf ** 2 + ydf ** 2)
-  v1_theta = math.atan2(pointSelf[1], pointSelf[0])
-  v2_theta = math.atan2(coordinate[1], coordinate[0])
-  angle = (v2_theta - v1_theta) * (180.0 / math.pi)
-  #angle = np.rad2deg(np.atan2(ydf, xdf))
+  #v1_theta = math.atan2(ydf, xdf)
+  #v2_theta = math.atan2(coordinate[1], coordinate[0])
+  #angle = (v2_theta - v1_theta) * (180.0 / math.pi)
+  angle = (np.arctan2(ydf, xdf))
+  if(angle < 0):
+    angle = math.pi - angle
   movement = [magnitude, angle]
   return movement
 
-def move(input_coordinates):
-  global total_angle, total_distance, angle, distance, pause, pointSelf, movement
 
-  movement = coordinates(input_coordinates)
-  if(movement[1]-total_angle > 5):
-    mySetpoint.linear.x = 0.
-    mySetpoint.angular.z = 0.5
-    total_angle = total_angle + angle
-  elif(movement[1]-total_angle > 0.0 and movement[1]-total_angle <= 5):
-    mySetpoint.linear.x = 0.
-    mySetpoint.angular.z = 0.25
-    total_angle = total_angle + angle
-
-  elif(movement[1]-total_angle <= 0.1 and pause < 50):
-    pause += 1
-    mySetpoint.linear.x = 0.
-    mySetpoint.angular.z = 0.
-
-  elif(movement[0]-total_distance > 0.0 and mySetpoint.angular.z == 0.):
-    if(movement[0] - total_distance > 0.1):
-      mySetpoint.linear.x = 0.5
-      mySetpoint.angular.z = 0.
-    elif(movement[0] - total_distance <= 0.1 and total_distance >= movement[0]):
-      mySetpoint.linear.x = 0.25
-      mySetpoint.angular.z = 0.
-    total_distance = total_distance + distance
-
-  else:
-    pause = 0
-    if(angle > 360):
-      angle = angle%360
-    mySetpoint.linear.x = 0.
-    mySetpoint.angular.z = 0.
-    total_distance = 0
-    pointSelf = input_coordinates
+  
   
 
 
@@ -100,38 +69,75 @@ if __name__=='__main__':
     rospy.Subscriber("/wr", Float32, callbackWr)
     rospy.Subscriber("/wl", Float32, callbackWl)
     debug = Vector3()
-    pointSelf = [2,0]
+    pointSelf = [0,0]
     
     movement = [0,0]#rospy.get_param("/distance")
 
     print("Controller Node is Running")
     start_time = rospy.get_time()
-    
+    step = 0
+    i = 0
     check = 0
     
     #Run the node
     while not rospy.is_shutdown():
 
+      #next_point = rospy.get_param("/point")
       dt = rospy.get_time() - start_time
       if(check < 10):
         dt = 0.0
       distance = mySetpoint.linear.x * dt 
-      angle = np.rad2deg(mySetpoint.angular.z * dt)
+      angle = (mySetpoint.angular.z * dt)
       input_coordinates = [[2,0], [2,2], [0,2], [0,0]]
       
-      if(pointSelf == [2,0]):
-        move(input_coordinates[1])
-      """
-      elif(pointSelf == input_coordinates[0]):
-        move(input_coordinates[1])
-      elif(pointSelf == input_coordinates[1]):
-        move(input_coordinates[2], mySetpoint)
-      elif(pointSelf == input_coordinates[2]):
-        move(input_coordinates[3], mySetpoint)"""
+
+
+      movement = coordinates(input_coordinates[step])
+      if(pointSelf == input_coordinates[step]):
+        step += 1
       
-      debug.x = pause
-      debug.y = movement[0]-total_distance
-      debug.z = movement[1]
+
+      if(movement[1]-total_angle > 0.1):
+        mySetpoint.linear.x = 0.
+        mySetpoint.angular.z = 0.5
+        total_angle = total_angle + angle
+        """elif(movement[1]-total_angle > 0.0 and movement[1]-total_angle <= 5):
+        mySetpoint.linear.x = 0.
+        mySetpoint.angular.z = 0.25
+        total_angle = total_angle + angle"""
+
+      elif(movement[1]-total_angle <= 0.1 and pause < 50):
+        pause += 1
+        mySetpoint.linear.x = 0.
+        mySetpoint.angular.z = 0.
+
+      elif(movement[0]-total_distance > 0.0 and mySetpoint.angular.z == 0.):
+        pause = 0
+        mySetpoint.linear.x = 0.5
+        mySetpoint.angular.z = 0.
+        """if(movement[0] - total_distance > 0.1):
+        elif(movement[0] - total_distance <= 0.1 and total_distance >= movement[0]):
+          mySetpoint.linear.x = 0.25
+          mySetpoint.angular.z = 0."""
+        total_distance = total_distance + distance
+      
+      elif(movement[1]-total_angle <= 0.1 and pause < 50 and movement[0]-total_distance <= 0):
+        pause += 1
+        mySetpoint.linear.x = 0.
+        mySetpoint.angular.z = 0.
+
+      else:
+        pause = 0
+        if(angle > math.pi):
+          angle = angle%math.pi
+        mySetpoint.linear.x = 0.
+        mySetpoint.angular.z = 0.
+        total_distance = 0
+        pointSelf = input_coordinates[step]
+
+      debug.x = movement[1]
+      debug.y = np.rad2deg(total_angle)
+      debug.z = total_distance
       #Write your code here
       pub.publish(mySetpoint)
       pub2.publish(debug)
