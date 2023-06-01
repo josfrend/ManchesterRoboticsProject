2ba3d9365063292e67d6709b0254a9491bbe9877
josfrend <75293152+josfrend@users.noreply.github.com>
2023-05-31 22:16:20 -0600

    Update color_identification.py

--- Implementacion de Robotica Inteligente/Week 8/color_identification.py
+++ Implementacion de Robotica Inteligente/Week 8/color_identification.py
@@ -13,20 +13,10 @@ import argparse
 class colorIdentificator():
     def __init__(self):
         self.image_sub = rospy.Subscriber('/video_source/raw', Image, self.image_callback)
-        self.red_pub = rospy.Publisher('/red_traffic_light', Image, queue_size=10)
-        self.yellow_pub = rospy.Publisher('/yellow_traffic_light', Image, queue_size=10)
-        self.green_pub = rospy.Publisher('/green_traffic_light', Image, queue_size=10)
         self.light_pub = rospy.Publisher('/light_color', String, queue_size=10)
         self.light_color = 'n'
-        self.last_color = 'n'
         self.bridge = CvBridge()
         rospy.on_shutdown(self.stop)
-
-
-        # parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
-        # parser.add_argument('--camera', help='Camera divide number.', default=0, type=int)
-        # args = parser.parse_args()
-        # self.cap = cv.VideoCapture(args.camera)
 
         #Define threshold values for traffic light detection
         self.cv_image = None
@@ -82,58 +72,50 @@ class colorIdentificator():
         self.yellow_count = 0
         self.green_count = 0
 
-    def blobs(self,val):
+    def blobs(self, val):
+        # Apply erosion and dilatation to the image 
         element = cv.getStructuringElement(cv.MORPH_RECT, (2 * self.erosion_size + 1, 2 * self.erosion_size + 1),(self.erosion_size, self.erosion_size))
         erosion_dst = cv.erode(val, element)
         element = cv.getStructuringElement(cv.MORPH_RECT, (2 * self.dilatation_size + 1, 2 * self.dilatation_size + 1),(self.dilatation_size, self.dilatation_size))
         dilatation_dst = cv.dilate(erosion_dst, element)
 
-
+        # Invert the image to adjust for the blob detector
         dilatation_dst = 255-dilatation_dst
+        # Use blob detector
         keypoints = self.detector.detect(dilatation_dst)
-
         
-        #blank = np.zeros((1, 1))
-        #blobs_d = cv.drawKeypoints(dilatation_dst, keypoints, blank, (255, 0, 0),cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        #cv.putText(blobs_d, str(len(keypoints)), (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-        #return blobs_d, len(keypoints)
         return len(keypoints)
 
     def image_callback(self,data):
+        # Open image through Cv Bridge
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         except CvBridgeError as e:
             print(e)
             return
-
-        #ret, frame = self.cap.read()
-        #if frame is not None:
+        # Turn image from BGR to HSV and apply the color masks
         frame_HSV = cv.cvtColor(cv_image, cv.COLOR_BGR2HSV)
         red_HSV = cv.inRange(frame_HSV, (self.red_low_H, self.red_low_S, self.red_low_V), (self.red_high_H, self.red_high_S, self.red_high_V))
         red2_HSV = cv.inRange(frame_HSV, (self.red2_low_H, self.red2_low_S, self.red2_low_V), (self.red2_high_H, self.red2_high_S, self.red2_high_V))
         red = cv.add(red_HSV, red2_HSV)
         yellow_HSV = cv.inRange(frame_HSV, (self.yellow_low_H, self.yellow_low_S, self.yellow_low_V), (self.yellow_high_H, self.yellow_high_S, self.yellow_high_V))
         green_HSV = cv.inRange(frame_HSV, (self.green_low_H, self.green_low_S, self.green_low_V), (self.green_high_H, self.green_high_S, self.green_high_V))
-
+       
+        # Detect blobs in each color image
         self.red_count = self.blobs(red)
         self.yellow_count = self.blobs(yellow_HSV)
         self.green_count = self.blobs(green_HSV)
-        #self.red_pub.publish(self.bridge.cv2_to_imgmsg(filtered_image, "bgr8"))
-        #self.yellow_pub.publish(self.bridge.cv2_to_imgmsg(filtered_image2, "bgr8"))
-        #self.green_pub.publish(self.bridge.cv2_to_imgmsg(filtered_image3, "bgr8"))
 
     
     def run(self):    
         
+        # Assign the value of the traffic light color the camera sees, with the hierarchy such as red, yellow and green 
         if(self.red_count > 0):
             self.light_color = 'r'
-            self.last_color = 'r'
         elif(self.yellow_count > 0):
             self.light_color = "y"
-            self.last_color = "y"
         elif(self.green_count > 0):
             self.light_color = 'g'
-            self.last_color = "g"
         else:
             self.light_color = 'n'
 
