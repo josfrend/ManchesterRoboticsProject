be9e2923b30428c89ca074defd69d3f9e825c262
josfrend <75293152+josfrend@users.noreply.github.com>
2023-05-31 22:33:59 -0600

    Update line_identification.py

--- Implementacion de Robotica Inteligente/Week 8/line_identification.py
+++ Implementacion de Robotica Inteligente/Week 8/line_identification.py
@@ -17,20 +17,12 @@ class lineDetector():
         self.processed_image = rospy.Publisher('/line_detection', Image, queue_size=10)
         self.bridge = CvBridge()
         rospy.on_shutdown(self.stop)
-
+        # Define resized image desired dimensions
         self.desired_width = 640
         self.desired_height = 480
-
+        
+        # Variable to store the offset from the line
         self.last_offset = 300
-
-
-        # parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
-        # parser.add_argument('--camera', help='Camera divide number.', default=0, type=int)
-        # args = parser.parse_args()
-        # self.cap = cv.VideoCapture(args.camera)
-
-        #Define threshold values for traffic light detection
-        self.cv_image = None
 
         # Define erosion and dilation parameters for blob detection
         self.erosion_size = 7
@@ -44,31 +36,35 @@ class lineDetector():
 
 
     def image_callback(self,data):
+        # Read the image from the Cv Bridge
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         except CvBridgeError as e:
             print(e)
             return
-        
-        lower_white = np.array([0,0,0])
-        upper_white = np.array([110,110,110])
+       # Turn to image to monocrome and re-scale it 
         frame = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY)
         original_resized = cv.resize(frame, (self.desired_width, self.desired_height))
-
+        
+        # Set up and apply the mask to filter black shades from the image
+        lower_black = np.array([0,0,0])
+        upper_black = np.array([110,110,110])
         mask = cv.inRange(cv_image, lower_white, upper_white)
+        # Invert the image
         mask = 255 - mask
             
-        
+        # Resize, apply filters, erosion and dilatation to the image to exalt certain features in the image
         resized = cv.resize(mask, (self.desired_width, self.desired_height))
         gaussian = cv.GaussianBlur(resized, (7,7), 1)
         element = cv.getStructuringElement(cv.MORPH_RECT, (2 * self.erosion_size + 1, 2 * self.erosion_size + 1),(self.erosion_size, self.erosion_size))
         erosion_dst = cv.erode(gaussian, element)
         element = cv.getStructuringElement(cv.MORPH_RECT, (2 * self.dilatation_size + 1, 2 * self.dilatation_size + 1),(self.dilatation_size, self.dilatation_size))
         dilatation_dst = cv.dilate(erosion_dst, element)
+        
+        # Sum vertically through the image matrix in a lower and delimited region
         sum = np.sum(dilatation_dst[350:479,15:585], axis=0)
 
-
-
+        
         borders = cv.Canny(dilatation_dst[350:479,:], 50, 50)
         lines = cv.HoughLinesP(borders, 1, np.pi/180, threshold=50, maxLineGap=10)
 
